dc9110401ca19478875c46077e147df4ed6907f9
D3r3ck <devnull@localhost>
2012-05-30 17:16:57 +0200

    Fixed forms for new version of symfony core

--- Form/Type/TagsType.php
+++ Form/Type/TagsType.php
@@ -2,17 +2,17 @@
 namespace BSP\TagBundle\Form\Type;
   
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 use BSP\TagBundle\Form\DataTransformer\TagsTransformer;
  
 class TagsType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->prependClientTransformer(new TagsTransformer());
     }
  
-    public function getParent(array $options)
+    public function getParent()
     {
         return 'text';
     }
