c9b0074a53aa887d679bb7dedee5f04340d4c027
D3r3ck <devnull@localhost>
2012-11-21 00:17:18 +0100

    Edit Product (Devel)

--- DependencyInjection/BSPTagExtension.php
+++ DependencyInjection/BSPTagExtension.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Definition;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -21,5 +22,9 @@ class BSPTagExtension extends Extension
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        
+        $definition = new Definition('BSP\TagBundle\Extension\TagTwigExtension');
+        $definition->addTag('twig.extension');
+        $container->setDefinition('tag_twig_extension', $definition);
     }
 }
